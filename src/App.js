e8f0f5b770910df81ea555c9285253ab60edd427
Youssef Elshabrawy <yielshabrawy@gmail.com>
2022-08-02 13:03:46 +0200

    up and ready!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import data from './data/data';
 import './css/App.css';
 
 function App() {
+    let counter = data.length;
+    const cards = data.map((c) => {
+        counter--;
+        return (
+            <div>
+                <Card key={c.id} {...c} />
+                {counter !== 0 && <hr className="container" />}
+            </div>
+        );
+    });
     return (
         <div>
             <Navbar />
-            <Card />
+            {cards}
         </div>
     );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,20 @@
 import React from 'react';
 
-function Card() {
+function Card(props) {
     return (
         <div className="container">
-            <img
-                src="https://source.unsplash.com/WLxQvbMyfas"
-                alt=""
-                className="card--img"
-            />
+            <img src={props.imageUrl} alt="" className="card--img" />
             <div className="data">
                 <div className="info-row">
                     <img src="assets/spot.png" alt="" />
-                    <h4>JAPAN</h4>
-                    <a href="/">View on Google Maps</a>
+                    <h4>{props.location.toUpperCase()}</h4>
+                    <a href={props.googleMapsUrl}>View on Google Maps</a>
                 </div>
-                <h1>Mount Fuji</h1>
-                <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
-                <p>
-                    Mount Fuji is the tallest mountain in Japan, standing at
-                    3,776 meters (12,380 feet). Mount Fuji is the single most
-                    popular tourist site in Japan, for both Japanese and foreign
-                    tourists.
-                </p>
+                <h1>{props.title}</h1>
+                <h5>
+                    {props.startDate} - {props.endDate}
+                </h5>
+                <p>{props.description}</p>
             </div>
         </div>
     );
